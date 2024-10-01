cf3405acea7dd42431b3f0aec9aa6cb7cbc572c8
sunph5 <1079209370@qq.com>
2024-10-01 12:47:10 +0800

    优化格式

--- USER/main.c
+++ USER/main.c
@@ -1,42 +1,41 @@
-#include "led.h"
 #include "FreeRTOS.h"
-#include "task.h"
+#include "led.h"
+#include "stdio.h"
 #include "stm32f10x.h"
-#include "stdio.h"	
- 
+#include "task.h"
+
 /************************************************
  ALIENTEK战舰STM32开发板 FreeRTOS 实验2-1
- 跑马灯实验 
+ 跑马灯实验
  技术支持：www.openedv.com
- 淘宝店铺：http://eboard.taobao.com 
+ 淘宝店铺：http://eboard.taobao.com
  关注微信公众平台微信号："正点原子"，免费获取STM32资料。
- 广州市星翼电子科技有限公司  
+ 广州市星翼电子科技有限公司
  作者：正点原子 @ALIENTEK
 ************************************************/
 
-#define START_TASK_PRIO		1 		//任务优先级
-#define START_STK_SIZE		128 	//任务堆栈大小
-TaskHandle_t StartTask_Handler;		//任务句柄
-void start_task(void *pvParameters);//任务函数
+#define START_TASK_PRIO 1			 // 任务优先级
+#define START_STK_SIZE 128			 // 任务堆栈大小
+TaskHandle_t StartTask_Handler;		 // 任务句柄
+void start_task(void *pvParameters); // 任务函数
 
-#define LED0_TASK_PRIO		2		//任务优先级
-#define LED0_STK_SIZE		50 		//任务堆栈大小
-TaskHandle_t LED0Task_Handler;		//任务句柄
-void led0_task(void *pvParameters);		//任务函数
+#define LED0_TASK_PRIO 2			// 任务优先级
+#define LED0_STK_SIZE 50			// 任务堆栈大小
+TaskHandle_t LED0Task_Handler;		// 任务句柄
+void led0_task(void *pvParameters); // 任务函数
 
-#define LED1_TASK_PRIO		3 		//任务优先级
-#define LED1_STK_SIZE		50 		//任务堆栈大小
-TaskHandle_t LED1Task_Handler;		//任务句柄
-void led1_task(void *pvParameters);		//任务函数
-
+#define LED1_TASK_PRIO 3			// 任务优先级
+#define LED1_STK_SIZE 50			// 任务堆栈大小
+TaskHandle_t LED1Task_Handler;		// 任务句柄
+void led1_task(void *pvParameters); // 任务函数
 
 int main(void)
 {
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4
-//	uart_init(115200);	//初始化串口
-	LED_Init();		  	//初始化LED
+	// NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4
+	//	uart_init(115200);	//初始化串口
+	LED_Init(); // 初始化LED
 
-	//创建开始任务
+	// 创建开始任务
 	xTaskCreate((TaskFunction_t)start_task,
 				(const char *)"start task",
 				(uint16_t)START_STK_SIZE,
@@ -44,32 +43,31 @@ int main(void)
 				(UBaseType_t)START_TASK_PRIO,
 				(TaskHandle_t *)&StartTask_Handler);
 	vTaskStartScheduler();
- }
+}
 
-
-//开始任务函数
-void start_task(void*pvParameters)
+// 开始任务函数
+void start_task(void *pvParameters)
 {
-	taskENTER_CRITICAL();//进入临界段
-	//创建LED0任务
-	xTaskCreate((TaskFunction_t)led0_task,				//任务函数
+	taskENTER_CRITICAL(); // 进入临界段
+	// 创建LED0任务
+	xTaskCreate((TaskFunction_t)led0_task, // 任务函数
 				(const char *)"led0 task",
 				(uint16_t)LED0_STK_SIZE,
 				(void *)NULL,
 				(UBaseType_t)LED0_TASK_PRIO,
 				(TaskHandle_t *)&LED0Task_Handler);
-	//创建LED1任务
-	xTaskCreate((TaskFunction_t)led1_task,				//任务函数
+	// 创建LED1任务
+	xTaskCreate((TaskFunction_t)led1_task, // 任务函数
 				(const char *)"led1 task",
 				(uint16_t)LED1_STK_SIZE,
 				(void *)NULL,
 				(UBaseType_t)LED1_TASK_PRIO,
 				(TaskHandle_t *)&LED1Task_Handler);
-	vTaskDelete(StartTask_Handler);	//删除开始任务
-	taskEXIT_CRITICAL();			//退出临界区
+	vTaskDelete(StartTask_Handler); // 删除开始任务
+	taskEXIT_CRITICAL();			// 退出临界区
 }
 
-//LED0任务函数
+// LED0任务函数
 void led0_task(void *pvParameters)
 {
 	while (1)
@@ -79,8 +77,8 @@ void led0_task(void *pvParameters)
 	}
 }
 
-//LED1任务函数
-void led1_task(void* pvParameters)
+// LED1任务函数
+void led1_task(void *pvParameters)
 {
 	while (1)
 	{
@@ -91,24 +89,24 @@ void led1_task(void* pvParameters)
 	}
 }
 
-#pragma import(__use_no_semihosting)             
-//标准库需要的支持函数                 
-struct __FILE 
-{ 
-	int handle; 
+#pragma import(__use_no_semihosting)
+// 标准库需要的支持函数
+struct __FILE
+{
+	int handle;
+};
 
-}; 
-
-FILE __stdout;       
-//定义_sys_exit()以避免使用半主机模式    
-_sys_exit(int x) 
-{ 
-	x = x; 
-} 
-//重定义fputc函数 
+FILE __stdout;
+// 定义_sys_exit()以避免使用半主机模式
+_sys_exit(int x)
+{
+	x = x;
+}
+// 重定义fputc函数
 int fputc(int ch, FILE *f)
-{      
-	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
-    USART1->DR = (u8) ch;      
+{
+	while ((USART1->SR & 0X40) == 0)
+		; // 循环发送,直到发送完毕
+	USART1->DR = (u8)ch;
 	return ch;
 }
